619537441ae55c44f0293dcea607041e7c3565a4
Jonathan <2236000300@utna.edu.mx>
2025-05-31 22:44:26 -0600

    Se agregaron ventanas emergentes con mas informacion sobre cada seccion, tambien implementadas con SweetAlert2

--- index.js
+++ index.js
@@ -49,4 +49,49 @@ const observer = new IntersectionObserver((entries) => {
 
 cards.forEach(card => observer.observe(card));
 
+/* .card {
+        opacity: 0;
+        transform: translateY(30px);
+        transition: all 0.6s ease;
+        }
+
+        .card.fade-in {
+        opacity: 1;
+        transform: translateY(0);
+    }
+*/
+
+function mostrarInfo(tipo) {
+  let titulo = '';
+  let texto = '';
+  let imagen = '';
+
+  switch (tipo) {
+    case 'solar':
+      titulo = 'Energía Solar';
+      texto = 'Ofrecemos instalación profesional de paneles solares con tecnología de punta, ideales para reducir tu huella de carbono y ahorrar en tu factura de electricidad.';
+      imagen = 'https://images.unsplash.com/photo-1613665813446-82a78c468a1d?q=80&w=2058&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+      break;
+    case 'domotica':
+      titulo = 'Domótica Ecológica';
+      texto = 'Automatiza tu hogar u oficina con sistemas inteligentes que te ayudan a controlar el consumo energético de manera eficiente y sostenible.';
+      imagen = 'https://images.unsplash.com/photo-1681263849578-7585abbe0455?q=80&w=2028&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+      break;
+    case 'consultoria':
+      titulo = 'Consultoría Verde';
+      texto = 'Te brindamos asesoría personalizada para transformar tus procesos tecnológicos hacia un modelo más sostenible y respetuoso con el medio ambiente.';
+      imagen = 'https://www.unir.net/wp-content/uploads/2021/11/cabecera-ambiente-min-1.jpg';
+      break;
+  }
+
+  Swal.fire({
+    title: titulo,
+    text: texto,
+    imageUrl: imagen,
+    imageWidth: 500,
+    imageAlt: titulo,
+    confirmButtonText: 'Cerrar',
+    confirmButtonColor: '#198754'
+  });
+}
 
